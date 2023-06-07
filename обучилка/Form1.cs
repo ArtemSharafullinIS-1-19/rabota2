bd5980aaf7f8f2f5da08a2220e2b1a3f620da9e9
tema9 <tema9@DESKTOP-O4S3FRG>
2023-06-07 22:46:01 +0500

    Добавьте файлы проекта.

---
+++ обучилка/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlConnector;
+
+namespace обучилка
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection connection = new MySqlConnection("host = 127.0.0.1; port = 3306; password =; user = root; database = obuch");
+
+            string login = textBox1.Text;
+            string pass = textBox2.Text;
+
+            string запрос = $"Select count(*) from Users where login = '{login}' and pass = '{pass}'";
+            connection.Open();
+            MySqlCommand command = new MySqlCommand(запрос, connection);
+            int count = Convert.ToInt32(command.ExecuteScalar());
+            if (count > 0)
+            {
+                MessageBox.Show("привет");
+                Form2 form2 = new Form2();
+                form2.Show();
+                Hide();
+            }
+            else
+            {
+                MessageBox.Show("пока");
+            }
+            connection.Close();
+        }
+    }
+}
---
+++ обучилка/Form2.cs
@@ -0,0 +1,49 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace обучилка
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Загрузи()
+        {
+            using (MySqlConnection connection = new MySqlConnection("host = 127.0.0.1; port = 3306; password =; user = root; database = obuch"))
+            {
+                connection.Open();
+                // Формируем запрос на выборку данных о сотрудниках
+                string запрос = "SELECT * from Tovar";
+
+                // Создаем команду для выполнения запроса
+                MySqlCommand command = new MySqlCommand(запрос, connection);
+                // Создаем адаптер для получения данных и заполняем таблицу DataGridView
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Загрузи();
+        }
+    }
+}
